3295dd866b9c303b5e9608e6bcc9077fcfd8bf98
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-03-18 16:10:43 +0530

    Add files via upload

---
+++ IRsensor.ino
@@ -0,0 +1,23 @@
+
+void setup() 
+{
+  pinMode (2, INPUT); // sensor pin INPUT
+  Serial.begin(9600);
+  pinMode (13, OUTPUT); // Led pin OUTPUT
+}
+
+void loop()
+{
+  Serial.print("IRSensorip  ");
+  Serial.println(digitalRead(2));
+  
+  if (digitalRead(2)==0)
+  {
+    digitalWrite(13, LOW); // LED LOW
+  }
+    else
+  {
+    digitalWrite(13, HIGH); // LED High
+  }
+  
+}
